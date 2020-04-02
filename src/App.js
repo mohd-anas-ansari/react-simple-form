18f918ba41fb5b2c6dbfb89e1a1537c6234480f5
Anas Ansari <anasansari157@gmail.com>
2020-04-02 14:41:26 +0530

    Passing data through props

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
-import React from 'react';
-import Table from './Table'
+import React from "react";
+import Table from "./Table";
 
 //App component
 class App extends React.Component {
-  state = {  }
-  render() { 
-    return ( 
-      <div className='container'>
-        <Table />
-      </div>
-     );
-  }
+	state = {};
+	render() {
+		const characters = [
+			{
+				name: "Charlie",
+				job: "Janitor"
+			},
+			{
+				name: "Mac",
+				job: "Bouncer"
+			},
+			{
+				name: "Dee",
+				job: "Aspring actress"
+			},
+			{
+				name: "Dennis",
+				job: "Bartender"
+			}
+		];
+		return (
+			<div className="container">
+				<Table charactersData={characters} />
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/Table.js
+++ src/Table.js
@@ -3,35 +3,41 @@ import React from "react";
 class Table extends React.Component {
 	state = {};
 	render() {
+    const { charactersData } = this.props;
+
 		return (
 			<table>
-				<thead>
-					<tr>
-						<th>Name</th>
-						<th>Job</th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr>
-						<td>Charlie</td>
-						<td>Janitor</td>
-					</tr>
-					<tr>
-						<td>Mac</td>
-						<td>Bouncer</td>
-					</tr>
-					<tr>
-						<td>Dee</td>
-						<td>Aspiring actress</td>
-					</tr>
-					<tr>
-						<td>Dennis</td>
-						<td>Bartender</td>
-					</tr>
-				</tbody>
+				<TableHeader />
+				<TableBody charactersData = {charactersData} />
 			</table>
 		);
 	}
 }
+
+const TableHeader = () => {
+	return (
+		<thead>
+			<tr>
+				<th>Name</th>
+				<th>Job</th>
+			</tr>
+		</thead>
+	);
+};
+
+const TableBody = (props) => {
+	console.log(props);
+
+  const rows = props.charactersData.map((row, index) => {
+    return (
+      <tr key={index}>
+        <td>{row.name}</td>
+        <td>{row.job}</td>
+      </tr>
+    )
+  });
+    return <tbody>{rows}</tbody>;
+};
+
 
 export default Table;
